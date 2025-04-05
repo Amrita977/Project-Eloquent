b476030d86acaa3417e898ce87ccdfc16654f342
Amrita977. <Amritarajbansh@gmail.com>
2025-04-05 12:21:59 +0530

    updated env file

---
+++ project_tables/app/Http/Controllers/StudentController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class StudentController extends Controller
+{
+    //
+
+    public function showStudents(){
+        $students = DB::table('students')
+         ->join('cities','student.city','=','cities.id')
+         ->get();
+
+         //return $students;
+         return view('welcome',compact('students'));
+    }
+}
